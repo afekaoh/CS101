6e9e0b68c257da1fbf92c98d2207a047d8e275f0
afekaoh <50743751+afekaoh@users.noreply.github.com>
2019-11-22 14:28:41 +0200

    Add the whole object

---
+++ HomeWork/cmake-build-debug/CMakeFiles/HomeWork.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork.dir/ass2.c.o"
+  "HomeWork.exe"
+  "HomeWork.pdb"
+  "libHomeWork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HomeWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
