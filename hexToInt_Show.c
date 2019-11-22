d8e3bdeecabfd153bce17cb8ca7865397abacf59
Adam Shapira <afekaoh@gmail.com>
2019-11-22 17:50:08 +0200

    adding new program

--- hexToInt_Show.c
+++ hexToInt_Show.c
@@ -2,13 +2,14 @@
 
 int main(){
 char a='a',b='b';
-int hexToInt;
+int hexToInt,hexA,hexB;
 printf("the letter a is: %c the letter b is: %c\n",a,b);
 printf("the ascii value of a is: %d and of b is: %d\n",a,b);
 hexToInt=a-10;
-a-=hexToInt;
-b-=hexToInt;
-printf("the value of a in hex is: %d and the value of b is: %d\n",a,b);
+hexA=a-hexToInt;
+hexB=b-hexToInt;
+printf("the diff between 'a' to a is: %d\n",hexToInt);
+printf("the value of a in hex is: %d and the value of b is: %d\n",hexA,hexB);
 
     return 0;
 }
