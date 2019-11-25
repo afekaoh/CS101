8753a20c26a1ba2417d1c5eb3a0db462f4140ea4
Adam Shapira <afekaoh@gmail.com>
2019-11-25 11:12:45 +0200

    some optimization and better documentation.

--- hexToInt_Show.c
+++ hexToInt_Show.c
@@ -6,10 +6,10 @@ int hexToInt,hexA,hexB;
 printf("the letter a is: %c the letter b is: %c\n",a,b);
 printf("the ascii value of a is: %d and of b is: %d\n",a,b);
 hexToInt=a-10;
-hexA=a-hexToInt;
+hexA=a-a-10;
 hexB=b-hexToInt;
 printf("the diff between 'a' to a is: %d\n",hexToInt);
-printf("the value of a in hex is: %d and the value of b is: %d\n",hexA,hexB);
+printf("the value of a in hex is: %d and the value of b is: %d\n",a-(a-10),b-(a-10));
 
     return 0;
 }
