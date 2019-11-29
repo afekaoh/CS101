9fc72af2b10ed67b1865a0cf5615e49af2912d16
Adam Shapira <afekaoh@gmail.com>
2019-11-29 11:18:55 +0200

    finished recPow

--- ass3.c
+++ ass3.c
@@ -4,10 +4,11 @@ int counter;
 
 double iterPow(double base, int power){
     const double ONE=1.00;
-    counter=0;
+    counter=1;
     //is negative to know when the power is negative
     int isNeg=0;
     double result=1.00;
+   //edge cases
     if (power == 0)
         return 1;
     if(power == 1)
@@ -16,6 +17,7 @@ double iterPow(double base, int power){
         return infinity();
     if(base==0)
         return 0;
+    //x to the power of negative number y is 1/(x^-y)
     if(power<0){
         power=power*-1;
         isNeg=1;
@@ -30,12 +32,61 @@ double iterPow(double base, int power){
     return result;
 }
 
+double recPow(double base, int power){
+    static int countRec=1;
+    const double ONE=1;
+    int isNeg=0;
+    double result=1;
+    if(power==0)
+        return 1;
+    if(power==1||base==0)
+        return base;
+    if(power<0&&base==0)
+        return infinity();
+    if (power<0){
+        power*=-1;
+        isNeg=1;
+    }
+    countRec++;
+    result=base*recPow(base,power-1);
+    counter=countRec;
+    countRec=1;
+    if(isNeg)
+        return (ONE/result);
+    return result;
+
+}
+
+double recEffiPow(double base,int power){
+    static int countRec=1;
+    const double ONE=1;
+    int isNeg=0;
+    double result=1;
+    if(power==0)
+        return 1;
+    if(power==1||base==0)
+        return base;
+    if(power<0&&base==0)
+        return infinity();
+    if (power<0){
+        power*=-1;
+        isNeg=1;
+    }
+    countRec++;
+    
+
+}
 int main(){
     int y;
-    double x=1;
+    double x,result;
     for (int i = -5; i <6 ; ++i) {
-        x=1.5;
-        printf("the base is:%f, the exp is:%d, the result is:%f, and the counter is:%d\n",x,i,iterPow(x,i),counter);
+        x=0;
+        result=iterPow(x,i);
+        printf("the base is:%f, the exp is:%d, the result is:%f, and the counter is:%d\n",x,i,result,counter);
+        result=recPow(x,i);
+        printf("the base is:%f, the exp is:%d, the result is:%f, and the counter is:%d\n",x,i,result,counter);
+        printf("\n");
+
     }
 
 
