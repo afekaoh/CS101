922e0fc19e9706ab7b83e31d6cd0cb4217e33469
Adam Shapira <afekaoh@gmail.com>
2019-11-29 21:39:41 +0200

    added some coding style

--- ass3.c
+++ ass3.c
@@ -1,25 +1,37 @@
-/**********
-todo:
- coding style
-
-********/
+/*******************
+* Adam Shay Shapira
+* 316044809
+* 01
+* ass03
+********************/
 #include <math.h>
 #include <stdio.h>
+
 int counter;
 
 double iterPow(double base, int power) {
-    const double ONE = 1.00;
+    /*******************************************************************************
+    * Function name: iterPow
+    * Input: double base (valid value every number) - number to raise to the power
+    *        int power (valid input every integer) - the exponent
+    * Output: double (value every number)
+    * Function Operation: the function gets a base and an exponent and return the value
+    *                     of the base to the power of power in an iterative way
+    *******************************************************************************/
+    //constant that equals to infinity by the definition of IEEE
+    const double inf = 1.00/0.00;
     //is negative to know when the power is negative
     int isNeg = 0;
     double result = 1.00;
     //edge cases
     if (power == 0)
         return 1;
-    if (power == 1)
+    //x^1==x and 0^x=0 (x>0)
+    if (power == 1 || base == 0)
         return base;
-    if(power<0&&base==0)
-        return (double)1/0.0;
-    //x to the power of negative number y is 1/(x^-y)
+    // 0^-y y>0 is infinity
+    if (power < 0 && base == 0)
+        return inf;
     if (power < 0) {
         power = power * -1;
         isNeg = 1;
@@ -27,22 +39,31 @@ double iterPow(double base, int power) {
     for (int i = 0; i < power; ++i) {
         counter++;
         result *= base;
-
     }
     if (isNeg)
-        return (ONE / result);
+    //x to the power of negative number y is 1/(x^-y)
+        return (1.00 / result);
     return result;
 }
 
 double recPow(double base, int power) {
-    const double ONE = 1;
+    /*******************************************************************************
+    * Function name: recPow
+    * Input: double base (valid value every number) - number to raise to the power
+    *        int power (valid input every integer) - the exponent
+    * Output: double (value every number)
+    * Function Operation: the function gets a base and an exponent and return the value
+    *                     of the base to the power of power in an naive recursive way
+    *                     of x^y=x*x^(y-1)
+    *******************************************************************************/
+    const double inf = 1.00/0.00;
     int isNeg = 0;
     double result;
     if (power == 0)
         return 1;
-   if((power<0) && (base==0))
-       return (double)1/0.0;
-    if (power == 1)
+    if ((power < 0) && (base == 0))
+        return inf;
+    if (power == 1 || base==0)
         return base;
 
     if (power < 0) {
@@ -52,35 +73,48 @@ double recPow(double base, int power) {
     counter++;
     result = base * recPow(base, power - 1);
     if (isNeg)
-        return (ONE / result);
+        return (1.00 / result);
     return result;
 
 }
 
 double recEffiPow(double base, int power) {
-    const double ONE = 1;
+  /*******************************************************************************
+  * Function name: recPow
+  * Input: double base (valid value every number) - number to raise to the power
+  *        int power (valid input every integer) - the exponent
+  * Output: double (value every number)
+  * Function Operation: the function gets a base and an exponent and return the val
+  *                     of the base to the power of power in an efficient recursive way
+  *                     of x^y=x^(y/2)*x^(y/2)
+  *******************************************************************************/
+    const double inf = 1.00/0.00;
     int isNeg = 0;
     double isOdd = 0;
     double result;
     if (power == 0)
         return 1;
-    if(power<0&&base==0)
-        return (double)1/0.0;
+    if (power < 0 && base == 0)
+        return inf;
     if (power == 1)
         return base;
     if (power < 0) {
         power *= -1;
         isNeg = 1;
     }
+    //if y is odd return
     if (power % 2) {
+        //putting one x to the side to make the power even
         isOdd = base;
         power--;
     } else
         isOdd = 1;
     counter++;
+    //computing x^(y/2)
     result = recEffiPow(base, power / 2);
     if (isNeg)
-        return (ONE / (isOdd * result * result));
+        return (1.00 / (isOdd * result * result));
+    //computing x^(y/2)*x^(y/2) and in the case of odd y multiply in the x we put aside
     return (isOdd * result * result);
 
 
@@ -88,20 +122,38 @@ double recEffiPow(double base, int power) {
 
 
 int maxRow(int arr[], int size) {
+    /*******************************************************************************
+    * Function name: maxRow
+    * Input: int arr[] an array of integers
+    *        int size the size of the array
+    * Output: integer
+    * Function Operation: the function gets an 1D array and returning the the biggest number
+    *                     in the array
+    *******************************************************************************/
     int max = 0;
-    for (int j = 0; j < size; ++j) {
-        max = max > arr[j] ? max : arr[j];
+    for (int i = 0; i < size; ++i) {
+        max = max > arr[i] ? max : arr[i];
     }
 
     return max;
 }
 
 void printRow(int arr[], int size, int max) {
-
+    /*******************************************************************************
+    * Function name: printRow
+    * Input: int arr[] an array of integers
+    *        int size the size of the array
+    *        int max the biggest number of the array
+    * Output: none
+    * Function Operation: the function gets a 1D array and returning prints the array
+    *                     in an evenly distributed way by adding a zeros to make all the
+    *                     numbers the same size by the factor of max
+    *******************************************************************************/
     //the log will return number of digit -1
     int setSize = (int) log10(max) + 1;
     for (int i = 0; i < size; ++i) {
         printf("%0*d", setSize, arr[i]);
+        //adding a space after every number but the last element
         if (i != size - 1)
             printf(" ");
     }
@@ -109,53 +161,85 @@ void printRow(int arr[], int size, int max) {
 }
 
 void printArr2D(int arr[][MAX], int row, int column) {
+    /*******************************************************************************
+    * Function name: printArr2D
+    * Input: int arr[][] an array of integers
+    *        int row numbers of rows in the array
+    *        int column number of columns in the array
+    * Output: none
+    * Function Operation: the function gets a 2D array and returning prints the array
+    *                     in an evenly distributed way by adding a zeros to make all the
+    *                     numbers the same size by the factor of its biggest number
+    *******************************************************************************/
+    //an array of all the max numbers in every row
     int maxInRow[row], max;
     for (int i = 0; i < row; ++i) {
         maxInRow[i] = maxRow(arr[i], column);
     }
+    //computing the biggest number in the array
     max = maxRow(maxInRow, row);
-
+    //print every row
     for (int i = 0; i < row; ++i) {
         printRow(arr[i], column, max);
     }
 
 }
-void swap(int arr[],int i,int j){
-    int temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
+
+void swap(int arr[], int i, int j) {
+    /*******************************************************************************
+    * Function name: swap
+    * Input: int arr[] an array of integers
+    *        int i a valid index in the array
+    *        int j a valid index in the array
+    * Output: none
+    * Function Operation: the function gets an array and to indexes and swap the elements
+    *                     of the array in those indexes
+    *******************************************************************************/
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 
 
 }
+
 void sort(int arr[], int size) {
-    int isSwap=0;
-    do{
-        isSwap=0;
-    for (int i=0; i < size-1 ; i++) {
-        if (arr[i] > arr[i + 1]) {
-            swap(arr, i, i + 1);
-            isSwap = 1;
+    /*******************************************************************************
+    * Function name: swap
+    * Input: int arr[] an array of integers
+    *        int i a valid index in the array
+    *        int j a valid index in the array
+    * Output: none
+    * Function Operation: the function gets an array and to indexes and swap the elements
+    *                     of the array in those indexes
+    *******************************************************************************/
+    int isSwap = 0;
+    do {
+        isSwap = 0;
+        for (int i = 0; i < size - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr, i, i + 1);
+                isSwap = 1;
+            }
         }
-    }
-    }while(isSwap);
+    } while (isSwap);
 }
 
 
 int isPermutation(int arr1[], int size1, int arr2[], int size2) {
-    int sortedArr1[size1], sortedArr2[size2],isEqual=0;
-    if(size1!=size2||size1==0||size2==0)
+    int sortedArr1[size1], sortedArr2[size2], isEqual = 0;
+    if (size1 != size2 || size1 == 0 || size2 == 0)
         return 0;
 
-    for (int i = 0; i < size1 ; i++) {
-            sortedArr1[i] = arr1[i];
-            sortedArr2[i] = arr2[i];
-    }
-   sort(sortedArr1, size1);
-   sort(sortedArr2, size2);
     for (int i = 0; i < size1; i++) {
-    if(sortedArr1[i]==sortedArr2[i])
-        isEqual++;
+        sortedArr1[i] = arr1[i];
+        sortedArr2[i] = arr2[i];
+    }
+    sort(sortedArr1, size1);
+    sort(sortedArr2, size2);
+    for (int i = 0; i < size1; i++) {
+        if (sortedArr1[i] == sortedArr2[i])
+            isEqual++;
     }
     isEqual = isEqual == size1 ? 1 : 0;
-return isEqual;
+    return isEqual;
 }
