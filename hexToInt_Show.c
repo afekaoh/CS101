5116d19f5359accfafb15a3f8b36c7cfb2a41d79
Adam Shapira <afekaoh@gmail.com>
2019-12-07 22:47:46 +0200

    finalized ass3
    Signed-off-by: Adam Shapira <afekaoh@gmail.com>

--- hexToInt_Show.c
+++ hexToInt_Show.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-char a='a',b='b';
-int hexToInt,hexA,hexB;
-printf("the letter a is: %c the letter b is: %c\n",a,b);
-printf("the ascii value of a is: %d and of b is: %d\n",a,b);
-hexToInt=a-10;
-hexA=a-a-10;
-hexB=b-hexToInt;
-printf("the diff between 'a' to a is: %d\n",hexToInt);
-printf("the value of a in hex is: %d and the value of b is: %d\n",a-(a-10),b-(a-10));
-printf("%f",infinity());
+int main() {
+    char a = 'a', b = 'b';
+    int hexToInt, hexA, hexB;
+    printf("the letter a is: %c the letter b is: %c\n", a, b);
+    printf("the ascii value of a is: %d and of b is: %d\n", a, b);
+    hexToInt = a - 10;
+    hexA = a - a - 10;
+    hexB = b - hexToInt;
+    printf("the diff between 'a' to a is: %d\n", hexToInt);
+    printf("the value of a in hex is: %d and the value of b is: %d\n", a - (a - 10), b - (a - 10));
+    printf("%f", infinity());
 
     return 0;
 }
