08f1927706761780208c9a42f34b50dbdaff099c
Adam Shapira <afekaoh@gmail.com>
2020-01-24 13:36:26 +0200

    optimization
    Signed-off-by: Adam Shapira <afekaoh@gmail.com>

--- ass7/ass7.py
+++ ass7/ass7.py
@@ -2,54 +2,62 @@
 Name: Adam Shay Shapira
 ID: 316044809
 Group:01
-Assignment 07
+Assignment: 07
 """
 import csv
 
-HTML_SUFFIX = ".html"
-FIND_LINK = "href"
+LINK_SUFFIX = ".html"
+LINK_TAG = "href"
 
 
-def is_link_line(line):
-	if FIND_LINK in line:
-		return True
-	return False
-
-
-def is_link(line):
-	return str(line).endswith(HTML_SUFFIX)
-
-
-def read_new_page(link):
-	with open(link) as f:
-		data = f.readlines()
-		name = f.name
-		d = list(filter(is_link_line, data))
-		return name, d
-
-
-def find_links(page):
-	links = list()
-	for line in page:
+def find_links(line_list):
+	"""
+	:param line_list: a list of lines
+	:return: links: a list of links end with a link suffix
+	"""
+	links = set()
+	for line in line_list:
 		line = line.split('"')
-		line = list(filter(is_link, line))
+		line = filter(lambda word: word.endswith(LINK_SUFFIX), line)
 		for link in line:
-			if link not in links:
-				links.append(link)
-	return links
+			links.add(link)
+	return list(links)
+
+
+def parse_page(page):
+	"""
+	:param page: a page to search links in
+	:return: name: the name of the file
+	:return: link_list: a list of of all the lines with link tag in them
+	"""
+	with open(page) as file:
+		lines = file.readlines()
+		link_list = list(filter(lambda line: LINK_TAG in line, lines))
+		links = find_links(link_list)
+		return links
 
 
 def crawler(link, dic):
-	file_name, new_data = read_new_page(link)
-	links = find_links(new_data)
-	dic[file_name] = links
-	for link in links:
+	"""
+	:param link: string with a name of a file to open
+	:param dic: dictionary to write to
+	:return: the function open the file and adds in a recursive way all the links inside of it
+	"""
+	if link in dic:
+		return
+	links = parse_page(link)
+	dic[link] = links
+	for link in dic[link]:
 		if link not in dic:
 			crawler(link, dic)
 
 
 def create_csv(dic):
-	with open("result.csv", mode='w', newline='') as file:
+	"""
+	:param dic: a dictionary object
+	:return: the function takes the dictionary and create a results.csv file with the dictionary content
+	"""
+	with open("results.csv", mode='w', newline='') as file:
 		writer = csv.writer(file)
 		for key in dic:
 			line = [key, ]
